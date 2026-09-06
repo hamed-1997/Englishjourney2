809bfb75ba5b4a78bf2efa71105cda0006f2f1cb
hamed-1997 <98464810+hamed-1997@users.noreply.github.com>
2026-09-06 12:53:32 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "english-journey-v6";
+const CACHE = "english-journey-v9";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,6 +7,7 @@ const ASSETS = [
   "./js/curriculum-data.js",
   "./manifest.json",
   "./assets/logo.png",
+  "./assets/book-cover.png",
   "./assets/splash-icon.png",
   "./assets/splash-plane.png",
   "./assets/splash-text.png",
